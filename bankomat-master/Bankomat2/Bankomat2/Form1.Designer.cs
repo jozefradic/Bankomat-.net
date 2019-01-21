b5e3e3fff447c440e5ad4a38045d682a8eea92bb
Jojo <32668482+jozefradic@users.noreply.github.com>
2019-01-21 09:26:06 +0100

    Add files via upload

---
+++ bankomat-master/Bankomat2/Bankomat2/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Bankomat2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(162, 60);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(133, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(132, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(200, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(266, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(132, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(200, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 45);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(266, 164);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 45);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(132, 225);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 45);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(200, 225);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 45);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(266, 225);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 45);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.Location = new System.Drawing.Point(200, 286);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(51, 45);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button11.Location = new System.Drawing.Point(132, 286);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(51, 45);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "C";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button12.Location = new System.Drawing.Point(266, 286);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(51, 45);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "OK";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Attempts:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(261, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(70, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(318, 31);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Your card was blocked!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(12, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(432, 31);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "You get access to your account!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ bankomat-master/Bankomat2/Bankomat2/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Bankomat2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAttempts();
+            label3.Hide();
+            label4.Hide();
+            button12.Enabled = false;
+        }
+        String textboxText;
+        #region
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;            
+            textBox1.Text = textboxText + button1.Text;           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText+ button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText+button9.Text;
+        }       
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textboxText = textBox1.Text;
+            textBox1.Text = textboxText + button10.Text;            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            selectInformation();          
+        }
+#endregion
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           if(textBox1.TextLength == 4)
+            {
+                button12.Enabled = true;
+            }
+
+           if(textBox1.TextLength>=4)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+                button10.Enabled = false;
+            }
+           else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+                button9.Enabled = true;
+                button10.Enabled = true;
+            }
+        }
+
+        public void selectInformation()
+        {
+            string ConnectionString = "server = localhost; port = 3306; database = bankomat;user=root; password=1234";
+            string query = "SELECT code,attempCount,isBlocked from codeinformation";
+            
+            MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {                
+                databaseConnection.Open();      
+                reader = commandDatabase.ExecuteReader();              
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {                       
+                        Boolean isBlocked = reader.GetBoolean(2);
+                        String code = reader.GetString(0);
+                        int attempts = reader.GetInt32(1);
+                       
+                        if(code == textBox1.Text)
+                        {
+                            MessageBox.Show("Correct code!");
+                            label4.Show();                        
+                        }
+                        else
+                        {
+                            MessageBox.Show("Incorrect code!");
+                            updateAttempts();
+                            getAttempts();
+                            button1.Enabled = true;
+                            button2.Enabled = true;
+                            button3.Enabled = true;
+                            button4.Enabled = true;
+                            button5.Enabled = true;
+                            button6.Enabled = true;
+                            button7.Enabled = true;
+                            button8.Enabled = true;
+                            button9.Enabled = true;
+                            button10.Enabled = true;
+                            textBox1.Text = "";
+                        }
+
+                        if(attempts==1)
+                        {
+                            blockCard();
+                            label3.Show();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }              
+
+                // Finally close the connection
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void updateAttempts()
+        {
+            string ConnectionString = "server = localhost;port = 3306; database = bankomat;user=root; password= 1234";
+            string query = "UPDATE codeinformation set attempCount= attempCount - 1 ";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            databaseConnection.Open();
+
+            // Execute the query
+            reader = commandDatabase.ExecuteReader();
+        }
+
+        public void blockCard()
+        {
+            string ConnectionString = "server = localhost; port = 3306; database = bankomat;user=root; password=1234";
+            string query = "UPDATE codeinformation set isBlocked= true";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            databaseConnection.Open();
+
+            // Execute the query
+            reader = commandDatabase.ExecuteReader();
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;         
+        }
+
+        public void getAttempts()
+        {
+            string ConnectionString = "server = localhost; port = 3306; database = bankomat;user=root; password=1234";
+            string query = "SELECT attempCount from codeinformation";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(ConnectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();                
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int attempts = reader.GetInt32(0);
+                        label2.Text = attempts.ToString();                                    
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                // Finally close the connection
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
